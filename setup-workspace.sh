a58ec754cff90d541660ec66e99d1a431ca3fbc4
indirakalagara <kindira@gmail.com>
2022-07-22 19:12:47 +0530

    portworx, gitops, devtools added to std arc
    Signed-off-by: indirakalagara <kindira@gmail.com>

--- setup-workspace.sh
+++ setup-workspace.sh
@@ -7,6 +7,8 @@ FLAVOR="quickstart"
 STORAGE=""
 PREFIX_NAME="std"
 REGION="ap-south-1"
+GITOPS=""
+
 
 Usage()
 {
@@ -18,12 +20,14 @@ Usage()
    echo "  s     the storage option to use (portworx or odf)"
    echo "  n     (optional) prefix that should be used for all variables"
    echo "  r     (optional) the region where the infrastructure will be provisioned"
+   echo "  g     Install gitops server on OCP cluster"
+   echo "  d     Install cloud-native developmemt toolkit on OCP cluster "
    echo "  h     Print this help"
    echo
 }
 
 # Get the options
-while getopts ":f:s:n:r:" option; do
+while getopts ":f:s:n:r:gd" option; do
    case $option in
       h) # display Help
          Usage
@@ -36,6 +40,10 @@ while getopts ":f:s:n:r:" option; do
          PREFIX_NAME=$OPTARG;;         
       r) # Enter a name
          REGION=$OPTARG;;         
+      g) 
+         GITOPS="gitops";;         
+      d) 
+         DEVTOOLS="tools";;         
      \?) # Invalid option
          echo "Error: Invalid option"
          Usage
@@ -43,6 +51,8 @@ while getopts ":f:s:n:r:" option; do
    esac
 done
 
+echo "GITOPS : ${GITOPS}"
+echo "DEVTOOLS : ${DEVTOOLS}"
 
 if [[ -z "${FLAVOR}" ]]; then
   FLAVORS=($(find "${SCRIPT_DIR}" -type d -maxdepth 1 | grep "${SCRIPT_DIR}/" | sed -E "s~${SCRIPT_DIR}/~~g" | grep -E "^[0-9]-" | sort | sed -e "s/[0-9]-//g" | awk '{$1=toupper(substr($1,0,1))substr($1,2)}1'))
@@ -70,6 +80,8 @@ fi
 
 STORAGE_OPTIONS=($(find "${SCRIPT_DIR}/${FLAVOR_DIR}" -type d -maxdepth 1 -name "210-*" | grep "${SCRIPT_DIR}/${FLAVOR_DIR}/" | sed -E "s~${SCRIPT_DIR}/${FLAVOR_DIR}/~~g" | sort))
 
+echo "STORAGE_OPTIONS : ${STORAGE_OPTIONS}"
+
 if [[ -z "${STORAGE}" ]]; then
 
   PS3="Select the storage: "
@@ -88,6 +100,21 @@ else
     fi
   done
 fi
+
+if [[ -n "${GITOPS}" ]]; then
+  GITOPS=($(find "${SCRIPT_DIR}/${FLAVOR_DIR}" -type d -maxdepth 1 -name "200-*" | grep "${SCRIPT_DIR}/${FLAVOR_DIR}/" | sed -E "s~${SCRIPT_DIR}/${FLAVOR_DIR}/~~g" | sort))
+  
+fi
+echo "GITOPS : ${GITOPS}"
+
+echo "DEVTOOLS : ${DEVTOOLS}"
+
+if [[ -n "${DEVTOOLS}" ]]; then
+  DEVTOOLS=($(find "${SCRIPT_DIR}/${FLAVOR_DIR}" -type d -maxdepth 1 -name "220-*" | grep "${SCRIPT_DIR}/${FLAVOR_DIR}/" | sed -E "s~${SCRIPT_DIR}/${FLAVOR_DIR}/~~g" | sort))
+  
+fi
+echo "DEVTOOLS : ${DEVTOOLS}"
+
 
 WORKSPACES_DIR="${SCRIPT_DIR}/../workspaces"
 WORKSPACE_DIR="${WORKSPACES_DIR}/current"
@@ -132,6 +159,14 @@ do
   fi
 
   if [[ "${name}" =~ ^210 ]] && [[ "${name}" != "${STORAGE}" ]]; then
+    continue
+  fi
+
+  if [[ "${name}" =~ ^200 ]] && [[ "${name}" != "${GITOPS}" ]]; then
+    continue
+  fi
+
+  if [[ "${name}" =~ ^220 ]] && [[ "${name}" != "${DEVTOOLS}" ]]; then
     continue
   fi
 
